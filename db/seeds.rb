2ba4878bb29e05ef098a77914c0e1bf0fdbc1b20
davidauza.engineer <davidauza01@gmail.com>
2020-05-20 20:31:36 -0500

    Create microservice structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+  FreelanceDocument.create!(
+      title: "Document #{n}",
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+      file_url: 'https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit?usp=sharing',
+      image_url: 'https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg'
+  )
+end
